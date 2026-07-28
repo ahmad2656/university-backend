8aa5a98a20808f4afaeaf4bfc8b0a0a92312deec
ahmad2656 <hashmi565758@gmail.com>
2026-07-28 18:28:54 +0500

    Fix CORS

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
@@ -21,6 +21,17 @@ app.use(cors());
 //     credentials: true,
 //   }),
 // );
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://university-frontend-nu.vercel.app/",
+      /\.vercel\.app$/,
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
@@ -32,13 +43,13 @@ app.use("/api/auth", authRoutes);
 
 app.use("/api/student", verifyToken, studentRoutes);
 app.use("/api/teacher", verifyToken, teacherRoutes);
-app.use("/api/admin", verifyToken, adminRoutes);      
-          
+app.use("/api/admin", verifyToken, adminRoutes);
+
 app.get("/", (req, res) => {
   res.json({ message: "University Portal API Running" });
 });
 
-app.use((err, req, res, next) => {                                                                                                                                                                                               
+app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
     success: false,
